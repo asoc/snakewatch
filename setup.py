b55cc57c8cf5795381851961e455ca5692478939
Alex Honeywell <alex.honeywell@gmail.com>
2012-11-14 23:04:37 -0500

    Setup settings for py2app in progress.

--- setup.py
+++ setup.py
@@ -4,12 +4,35 @@ import ez_setup
 ez_setup.use_setuptools()
 
 import os
-from setuptools import setup
+import sys
+from setuptools import setup, find_packages
 
 from snakewatch import NAME, VERSION, DESCRIPTION, URL, AUTHOR, AUTHOR_EMAIL
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+main = ['snakewatch/main.py',]
+pyside_packages = ['PySide.QtCore', 'PySide.QtGui',]
+
+if sys.platform.startswith('darwin'):
+    extra_options = dict(
+        setup_requires = ['py2app',],
+        app = main,
+        options = dict(py2app = dict(
+            argv_emulation = True,
+            includes = pyside_packages,
+        )),
+    )
+elif sys.platform.startswith('win'):
+    extra_options = dict(
+        setup_requires = ['py2exe',],
+        app = main,
+    )
+else:
+    extra_options = dict(
+        scripts = main,
+    )
 
 setup(
     name = NAME,
@@ -23,10 +46,9 @@ setup(
     keywords = 'log, tail',
     long_description = read('README.txt'),
 
-    packages = ['snakewatch', 'snakewatch.ui', 'snakewatch.input', 'snakewatch.action'],
+    packages = find_packages(),
     install_requires = ['colorama',],
-
-    app = ['Snakewatch.app',],
+    
     entry_points = {
         'console_scripts': [
             'snakewatch = snakewatch.main:main',
@@ -44,5 +66,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: System :: Logging',
-    ]
+    ],
+    
+    **extra_options
 )
